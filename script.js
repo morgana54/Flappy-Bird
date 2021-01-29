6510801bba59c27e0c1a7429732e0463dc2208a7
Jakub Pietrzak <jpietrzi@gmail.com>
2021-01-29 14:37:15 +0100

    Add animations to heading and platypus + begin working on start screen

--- script.js
+++ script.js
@@ -15,7 +15,10 @@ hole.addEventListener('animationiteration', () => {
 })
 
 // Make platypus jump
-window.addEventListener("keydown", jump)
+window.addEventListener("keydown", jump) 
+
+// Start the game
+window.addEventListener("click", startGame) // Later after asking Kacper make some distinct buttons to be pressed to call this function
 
 // Gravity
 setInterval(() => { 
@@ -43,8 +46,10 @@ setInterval(() => {
         state.score = 0;
         state.fallCount = 0;
         
+        
         // Restart animation after alert has been closed
         setTimeout(() => {
+            // document.getElementById('alert').style.display = 'flex'
             obstacle.style.animation = "obstacle 4s linear infinite";
             hole.style.animation = "obstacle 4s linear infinite";
             randomHole(hole)
@@ -60,6 +65,7 @@ setInterval(() => {
 function jump() {
     state.jumping = 1; // Stop platypus from falling down
     let jumpCount = 0;
+    platypus.style.transform = 'rotate(-18deg)' // More realistic jump
     let jumpInterval = setInterval(() => {
         let platypusTop = 
         parseInt(window.getComputedStyle(platypus).getPropertyValue("top"));
@@ -71,6 +77,7 @@ function jump() {
             platypus.style.top = `${platypusTop-2}px`
         }
         if(jumpCount > 26) {
+            platypus.style.transform = 'none'
             clearInterval(jumpInterval);
             state.jumping = 0;
             jumpCount = 0;
@@ -114,31 +121,35 @@ function containsInHeight(elemA, elemB) {
     return ((topA > topB) && (topA + heightA < topB + heightB)) ? true : false
 }
 
-/* DODAWAĆ TEŻ JAKIEŚ FAJNE ANIMACJE I LEPSZE STYLE DO TEJ GRY ŻEBY BYŁA ŻYWA!!!!!!!!!!!!!!!!!!!!!!! transition properties 
-- do heading dasz coś fajnego ze zmianą kształtu, koloru, cienia i wielkości!!!
-- dodać instrukcję grania krótką
-- custom alert
-- dodać animację fajną jak będzie skok czy coś, jakiś zwykły rotate wystarczy
-- dodać animację umierającego dziobaka (np. nagle clearujesz interwały i potem odpalasz jakąś animację z css --> ale najpierw spytaj Kacpra co sądzi o twoich pomysłach, żbey efektywnie pracować)
-  // // - poziom trudności: ZAPYTAĆ KACPRA NAJPIERW BO TOBIE COŚ NIE SZŁO!!!!! a on pewnie wie od razu
-    // // - zmniejszać dziury lekko co jakiś score
-    // // - przyspieszać lekko animację co jakiś score!!
+function startGame() {
+    document.getElementById('alert').style.display = 'none'
+}
 
-    // // Increase level of diffculty
-    // if(state.score > 1) {
-    //     obstacle.style.animation = "obstacle 3s linear infinite";
-    //     hole.style.animation = "obstacle 3s linear infinite";
-    //     hole.style.height = 155 + 'px'
-    // }
-- zrobić tak żeby zawsze dziobak umierał jeśli się dokładnie styka z obstacle (miałeś pomysł na sleep function, ale moze np. wgle nie bedzie potrzebne po zrobieniu animacji itp.)
-*/
 
-// WZÓR NA PISANIE GIER OD KACPRA
-// const state = {}
+// /* DODAWAĆ TEŻ JAKIEŚ FAJNE ANIMACJE I LEPSZE STYLE DO TEJ GRY ŻEBY BYŁA ŻYWA!!!!!!!!!!!!!!!!!!!!!!! transition properties 
+// CURRENT TASK: figure out how to make (using Timeout and after watching videos and talking with Kacper) start screen reappear and freeze the game until mouse is clicked
+// - make nice animation of hiding the box, and after animation is done: start the game (so the interval)
+// stop the heading animation after start of the game
+// - dodać instrukcję grania krótką
+// - custom alert na koniec pokazujący wynik i game over
+// - dodać animację umierającego dziobaka (np. nagle clearujesz interwały i potem odpalasz jakąś animację z css --> ale najpierw spytaj Kacpra co sądzi o twoich pomysłach, żbey efektywnie pracować)
+//   // // - poziom trudności: ZAPYTAĆ KACPRA NAJPIERW BO TOBIE COŚ NIE SZŁO!!!!! a on pewnie wie od razu
+//     // // - zmniejszać dziury lekko co jakiś score
+//     // // - przyspieszać lekko animację co jakiś score!!
 
-// function gameloop() {
-//     state = update(state)
-//     render(state)
-// }
+//     // // Increase level of diffculty
+//     // if(state.score > 1) {
+//     //     obstacle.style.animation = "obstacle 3s linear infinite";
+//     //     hole.style.animation = "obstacle 3s linear infinite";
+//     //     hole.style.height = 155 + 'px'
+//     // }
 
-// setInterval(gameloop, 10)
+// // WZÓR NA PISANIE GIER OD KACPRA
+// // const state = {}
+
+// // function gameloop() {
+// //     state = update(state)
+// //     render(state)
+// // }
+
+// // setInterval(gameloop, 10)
