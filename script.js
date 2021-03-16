10c17ceecc71ea1fc341d3d98736422f1b2d8e8d
Jakub Pietrzak <jpietrzi@gmail.com>
2021-03-16 11:15:49 +0100

    Add local highscore and fix startScreen

--- script.js
+++ script.js
@@ -5,62 +5,64 @@ const platypus = document.getElementById("platypus")
 const state = {
     jumping: 0,
     score: 0,
+    highscore: localStorage.getItem('highscore'),
     fallCount: 0
 }
-
 // Generate random hole
 hole.addEventListener('animationiteration', () => {
     randomHole(hole)
     state.score++; // add to score for beating the hole
 })
-
 // Make platypus jump
 window.addEventListener("keydown", jump) 
-
 // Start the game
 window.addEventListener("click", startGame) // Later after asking Kacper make some distinct buttons to be pressed to call this function
 
-// Gravity
-setInterval(() => { 
-    let platypusTop = parseInt(window.getComputedStyle(platypus).getPropertyValue("top"));
+function startGame() {
+    // Remove listener so that startGame function will only run once
+    window.removeEventListener("click", startGame)
+    document.getElementById('startScreen').style.display = 'none'
+    // This function has to be called to update HTML from LocalStarage right away and not already after first playing first game
+    updateHighScore()
+    // Gravity
+    setInterval(() => { 
+        let platypusTop = parseInt(window.getComputedStyle(platypus).getPropertyValue("top"));
+        // Acceleration 
+        if(state.fallCount > 50) {
+            platypusTop += 5;
+        } else if(state.fallCount > 25) {
+            platypusTop += 4;
+        } else {
+            platypusTop += 3; 
+        }
+        // If platypus is not jumping at the moment make him fall down
+        if(state.jumping == 0) {
+            platypus.style.top = `${platypusTop}px` 
+        }
 
-    // Acceleration 
-    if(state.fallCount > 50) {
-        platypusTop += 5;
-    } else if(state.fallCount > 25) {
-        platypusTop += 4;
-    } else {
-        platypusTop += 3; 
-    }
-
-    // If platypus is not jumping at the moment make him fall down
-    if(state.jumping == 0) {
-        platypus.style.top = `${platypusTop}px` 
-    }
-
-    if ((platypusTop > 485) || (intersects(platypus, obstacle) && !containsInHeight(platypus, hole))) {
-        alert("Game over! Score: " + state.score) // Alert stopuje wszystko z js na stronie!!
-        platypus.style.top = 100 + "px";
-        obstacle.style.animation = "none";
-        hole.style.animation = "none";
-        state.score = 0;
-        state.fallCount = 0;
-        
-        
+        // Game over
+        if ((platypusTop > 485) || (intersects(platypus, obstacle) && !containsInHeight(platypus, hole))) {
+            alert("Game over! Your score: " + state.score) // Alert stopuje wszystko z js na stronie!!
+            platypus.style.top = 100 + "px";
+            obstacle.style.animation = "none";
+            hole.style.animation = "none";
+            if(state.score > state.highscore) {
+                state.highscore = state.score;
+            }
+            updateHighScore()
+            state.score = 0;
+            state.fallCount = 0;
         // Restart animation after alert has been closed
         setTimeout(() => {
-            // document.getElementById('alert').style.display = 'flex'
             obstacle.style.animation = "obstacle 4s linear infinite";
             hole.style.animation = "obstacle 4s linear infinite";
             randomHole(hole)
         }, 10)
     }
-
-    // console.log(getComputedStyle(obstacle).getPropertyValue("left"))
-
     state.fallCount++;
     updateScore()
 }, 10)
+}
 
 function jump() {
     state.jumping = 1; // Stop platypus from falling down
@@ -97,9 +99,14 @@ function updateScore() {
     scoreBox.innerHTML = `Score: ${state.score}`
 }
 
+function updateHighScore() {
+    let highScoreBox = document.getElementById("highscore")
+    highScoreBox.innerHTML = `Highscore: ${state.highscore}`
+    localStorage.setItem('highscore', state.highscore)
+}
+
 // returns true or false depening on whether elements intersect or not
 function intersects(elemA, elemB) {
-   
     // use 'left' and 'width' property to determine this
     let leftB = parseInt(window.getComputedStyle(elemB).getPropertyValue("left"));
     let widthA = parseInt(window.getComputedStyle(elemA).getPropertyValue("width"));
@@ -114,23 +121,18 @@ function containsInHeight(elemA, elemB) {
     let topB = parseInt(window.getComputedStyle(elemB).getPropertyValue("top"));
     let heightA = parseInt(window.getComputedStyle(elemA).getPropertyValue("height"));
     let heightB = parseInt(window.getComputedStyle(elemB).getPropertyValue("height"));
-
     // Make the counting start point the same as the character's one (uzgodnienie height hole'a z tych 500px)
     topB = topB + 500 
 
     return ((topA > topB) && (topA + heightA < topB + heightB)) ? true : false
 }
 
-function startGame() {
-    document.getElementById('alert').style.display = 'none'
-}
-
 
 // /* DODAWAĆ TEŻ JAKIEŚ FAJNE ANIMACJE I LEPSZE STYLE DO TEJ GRY ŻEBY BYŁA ŻYWA!!!!!!!!!!!!!!!!!!!!!!! transition properties 
-// CURRENT TASK: figure out how to make (using Timeout and after watching videos and talking with Kacper) start screen reappear and freeze the game until mouse is clicked
-// - make nice animation of hiding the box, and after animation is done: start the game (so the interval)
-// stop the heading animation after start of the game
-// - dodać instrukcję grania krótką
+// - dodać instrukcję grania krótką + STEROWANIE TYLKO SPACJĄ (keyCode)
+// - make nice animation of hiding the box after pressing start (to the middle, lasting 0.5s)
+// - stop the heading animation after start of the game
+// - fajna animacja powiększania się score kiedy się zdobędzie punkt
 // - custom alert na koniec pokazujący wynik i game over
 // - dodać animację umierającego dziobaka (np. nagle clearujesz interwały i potem odpalasz jakąś animację z css --> ale najpierw spytaj Kacpra co sądzi o twoich pomysłach, żbey efektywnie pracować)
 //   // // - poziom trudności: ZAPYTAĆ KACPRA NAJPIERW BO TOBIE COŚ NIE SZŁO!!!!! a on pewnie wie od razu
